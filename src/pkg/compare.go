43636e19606dc8157d60eae0eb7fbd912e770f3b
Adam Gahverdiev <adam_gahverdiev@mail.ru>
2023-12-20 08:50:46 +0300

    wip: added new pkg for compare arrays

---
+++ src/pkg/compare.go
@@ -0,0 +1,17 @@
+package pkg
+
+import "sort"
+
+func CompareArrays(a, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	sort.Ints(a)
+	sort.Ints(b)
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
