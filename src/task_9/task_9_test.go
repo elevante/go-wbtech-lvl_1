3f7b9651968b6fc846c4e71e4dca0b349d29b066
Adam Gahverdiev <adam_gahverdiev@mail.ru>
2023-12-26 15:22:30 +0300

    wip: finished task 9

---
+++ src/task_9/task_9_test.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"go-wbtech-lvl_1/src/pkg"
+	"testing"
+)
+
+func TestConveyor(t *testing.T) {
+	testTable := []struct {
+		numbers []int
+		expect  []int
+	}{
+		{
+			numbers: []int{2, 4, 8},
+			expect:  []int{4, 8, 16},
+		},
+		{
+
+			numbers: []int{2000, 4000, 8000},
+			expect:  []int{4000, 8000, 16000},
+		},
+		{
+			numbers: []int{-2, -4, -8},
+			expect:  []int{-4, -8, -16},
+		},
+		{
+			numbers: []int{},
+			expect:  []int{},
+		},
+	}
+
+	for _, testCase := range testTable {
+		multiplyByTwo := conveyor(testCase.numbers)
+
+		if pkg.CompareArrays(multiplyByTwo, testCase.expect) == false {
+			t.Errorf("Incorrect result. Expect %d, got%d",
+				testCase.expect,
+				multiplyByTwo)
+
+		}
+
+	}
+}
