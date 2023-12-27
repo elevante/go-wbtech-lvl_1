5eab55a72e49d3a0647933260eed5a428e57be2b
Adam Gahverdiev <adam_gahverdiev@mail.ru>
2023-12-27 03:08:47 +0300

    fix: deleted spaces

--- src/task_12/task_12.go
+++ src/task_12/task_12.go
@@ -4,17 +4,15 @@ import "fmt"
 
 func main() {
 	fmt.Println(createSet([]string{"cat", "cat", "dog", "cat", "tree"}))
-
 }
 
 func createSet(array []string) []string {
 	result := make(map[string]bool)
 
 	for _, value := range array {
-
 		result[value] = true
-
 	}
+
 	keys := make([]string, 0, len(result))
 
 	for key := range result {
