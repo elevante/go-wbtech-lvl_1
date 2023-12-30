d63775457111d861abc521d8d9f31c733c878d1a
Adam Gahverdiev <adam_gahverdiev@mail.ru>
2023-12-30 21:30:34 +0300

    wip: finish task16

--- src/task16/task_16.go
+++ src/task16/task_16.go
@@ -1,64 +1,30 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-)
-
-type slice interface {
-	int | float64
-}
+import "fmt"
 
 func main() {
-	listInts := []int{1, 5, 3, 7, 9}
-	itemInt := 9
-	sort.Ints(listInts)
-
-	listFloats := []float64{1.0, 5.0, 3.0, 7.0, 9.0}
-	itemFloat := 9.0
-	sort.Float64s(listFloats)
-
-	fmt.Println(binarySearch(listInts, itemInt))
-	fmt.Println(binarySearch(listFloats, itemFloat))
-
-	// Implementation with built-in methods
-
-	iInt := sort.SearchInts(listInts, itemInt)
-	fmt.Println(iInt)
-
-	if iInt < len(listInts) && listInts[iInt] == itemInt {
-		fmt.Printf("found %d at index %d in listInt\n", itemInt, iInt)
-	} else {
-		fmt.Printf("%d not found in listInt\n", itemInt)
-	}
-
-	xFloat := 9.0
-	iFloat := sort.SearchFloat64s(listFloats, xFloat)
-	if iFloat < len(listFloats) && listFloats[iFloat] == itemFloat {
-		fmt.Printf("found %.1f at index %d in listFloat\n", itemFloat, iFloat)
-	} else {
-		fmt.Printf("%.1f not found in listFloat\n", itemFloat)
-	}
+	list := []int{2, 1, 4, 6, 8, 0}
+	fmt.Println(quikSort(list))
 }
 
-func binarySearch[V slice](list []V, item V) int {
-	var result int
-	low := 0
-	hight := len(list) - 1
-
-	for low <= hight {
-		mid := (low + hight) / 2
-		guess := list[mid]
-		if guess == item {
-			result = mid
-			break
+func quikSort(list []int) []int {
+	if len(list) < 2 {
+		return list
+	} else {
+		privot := list[0]
+		var less []int
+		var greater []int
+		for _, value := range list[1:] {
+			if value <= privot {
+				less = append(less, value)
+			}
+			if value > privot {
+				greater = append(greater, value)
+			}
 		}
-		if guess > item {
-			hight = mid - 1
-		}
-		if guess < item {
-			low = mid + 1
-		}
+		less = quikSort(less)
+		greater = quikSort(greater)
+		less = append(less, privot)
+		return append(less, greater...)
 	}
-	return result
 }
